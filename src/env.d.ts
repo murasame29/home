4997a170ed12780059f9f6c8c2001768db5ee6b3
murasame29 <oogiriminister@gmail.com>
2023-12-26 01:33:40 +0900

    やっぱこれしかないのか

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly NR_ACCOUNT_ID: string
-  readonly NR_TRUST_KEY: string
-  readonly NR_AGENT_ID: string
-  readonly NR_LICENSE_KEY: string
-  readonly NR_APPLICATION_ID: string
+  readonly VITE_NR_ACCOUNT_ID: string
+  readonly VITE_NR_TRUST_KEY: string
+  readonly VITE_NR_AGENT_ID: string
+  readonly VITE_NR_LICENSE_KEY: string
+  readonly VITE_NR_APPLICATION_ID: string
 }
 
 interface ImportMeta {
