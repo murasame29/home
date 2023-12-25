b204436106777977e7778b596209839b49695ee0
murasame29 <oogiriminister@gmail.com>
2023-12-26 01:26:43 +0900

    こうすれば秘匿できそう

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly NR_ACCOUNT_ID: string
+  readonly NR_TRUST_KEY: string
+  readonly NR_AGENT_ID: string
+  readonly NR_LICENSE_KEY: string
+  readonly NR_APPLICATION_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
